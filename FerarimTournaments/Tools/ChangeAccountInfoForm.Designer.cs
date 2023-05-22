bf0542f82ec65049401622936a209ee4d94e59b8
Maksym Kintor <kintormaksim@gmail.com>
2023-05-22 15:41:33 +0200

    implemented changeaccountinfo form design and basic reference back to accountinfo form

--- FerarimTournaments/Tools/ChangeAccountInfoForm.Designer.cs
+++ FerarimTournaments/Tools/ChangeAccountInfoForm.Designer.cs
@@ -28,18 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.changeNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.changeSurnameTextBox = new System.Windows.Forms.TextBox();
+            this.applyChangesBtn = new FerarimTournaments.LoginButton();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(243)))), ((int)(((byte)(145)))));
+            this.label2.Location = new System.Drawing.Point(139, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Name:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.panel2.Controls.Add(this.changeNameTextBox);
+            this.panel2.Location = new System.Drawing.Point(334, 124);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.panel2.Size = new System.Drawing.Size(467, 49);
+            this.panel2.TabIndex = 7;
+            // 
+            // changeNameTextBox
+            // 
+            this.changeNameTextBox.AcceptsReturn = true;
+            this.changeNameTextBox.AcceptsTab = true;
+            this.changeNameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.changeNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.changeNameTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.changeNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeNameTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.changeNameTextBox.Location = new System.Drawing.Point(13, 12);
+            this.changeNameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.changeNameTextBox.Name = "changeNameTextBox";
+            this.changeNameTextBox.Size = new System.Drawing.Size(441, 23);
+            this.changeNameTextBox.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(243)))), ((int)(((byte)(145)))));
+            this.label3.Location = new System.Drawing.Point(139, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 37);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Surname:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.panel3.Controls.Add(this.changeSurnameTextBox);
+            this.panel3.Location = new System.Drawing.Point(334, 231);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.panel3.Size = new System.Drawing.Size(467, 49);
+            this.panel3.TabIndex = 9;
+            // 
+            // changeSurnameTextBox
+            // 
+            this.changeSurnameTextBox.AcceptsReturn = true;
+            this.changeSurnameTextBox.AcceptsTab = true;
+            this.changeSurnameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.changeSurnameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.changeSurnameTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.changeSurnameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeSurnameTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.changeSurnameTextBox.Location = new System.Drawing.Point(13, 12);
+            this.changeSurnameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.changeSurnameTextBox.Name = "changeSurnameTextBox";
+            this.changeSurnameTextBox.Size = new System.Drawing.Size(441, 23);
+            this.changeSurnameTextBox.TabIndex = 0;
+            // 
+            // applyChangesBtn
+            // 
+            this.applyChangesBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(203)))), ((int)(((byte)(145)))));
+            this.applyChangesBtn.FlatAppearance.BorderSize = 0;
+            this.applyChangesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.applyChangesBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.applyChangesBtn.ForeColor = System.Drawing.Color.White;
+            this.applyChangesBtn.Location = new System.Drawing.Point(146, 500);
+            this.applyChangesBtn.Name = "applyChangesBtn";
+            this.applyChangesBtn.Size = new System.Drawing.Size(235, 55);
+            this.applyChangesBtn.TabIndex = 11;
+            this.applyChangesBtn.Text = "Apply Changes";
+            this.applyChangesBtn.UseVisualStyleBackColor = false;
+            this.applyChangesBtn.Click += new System.EventHandler(this.applyChangesBtn_Click);
             // 
             // ChangeAccountInfoForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(43)))), ((int)(((byte)(84)))));
+            this.ClientSize = new System.Drawing.Size(1140, 733);
+            this.Controls.Add(this.applyChangesBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
             this.Name = "ChangeAccountInfoForm";
             this.Text = "ChangeAccountInfoForm";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox changeNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox changeSurnameTextBox;
+        private LoginButton applyChangesBtn;
     }
 }
